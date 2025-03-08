55093020db414983ca3fee4f5304c30a9150ec92
SameerAli678 <alikhansameer234@gmail.com>
2025-03-09 00:04:31 +0500

    home page push

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+// src/app/page.tsx
+
+// HomeView Import
+// import LoginView from '@/views/Auth/login';
+// import PasswordView from '@/views/Auth/password';
+import HomeView from '@/views/Home';
+
+const Home = () => <HomeView />;
+
+export default Home;
+// #9995AC
