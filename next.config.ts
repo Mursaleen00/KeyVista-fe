3bbcbe553eb7665d2bb39765cd1fa7939c867550
SameerAli678 <alikhansameer234@gmail.com>
2025-03-25 04:09:20 +0500

    - creating the properties details page - create a component properties-price - create a component property-features - create a component features-amenities - Font family use in main layout

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
 export default nextConfig;
+const nextConfigs = {
+  images: {
+    domains: ['placehold.co'], // Allowed image domains
+  },
+};
+
+module.exports = nextConfigs;
