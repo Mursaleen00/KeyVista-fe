dd8d24eda7a55b750ef3411345c814dee45e5776
SameerAli678 <alikhansameer234@gmail.com>
2025-03-29 04:33:07 +0500

    - My properties card completed - product card horizontal completed - my property page completed - some files deleted

--- src/views/My-Property/index.tsx
+++ src/views/My-Property/index.tsx
@@ -1,9 +1,23 @@
 // src/app/views/My-Properties/index.tsx
 
+import MyPropertyCard from '@/components/cards/my-property-card';
+import Title from '@/components/common/title';
 import React from 'react';
 
 const MyPropertiesView = () => {
-  return <div>MyPropertiesView</div>;
+  return (
+    <div className='grid gap-y-'>
+      <div className='grid sm:py-9 sm:px-28'>
+        <Title text='My Properties' />
+      </div>
+      <div className='grid justify-center gap-y-6'>
+        <MyPropertyCard />
+        <MyPropertyCard />
+        <MyPropertyCard />
+        <MyPropertyCard />
+      </div>
+    </div>
+  );
 };
 
 export default MyPropertiesView;
