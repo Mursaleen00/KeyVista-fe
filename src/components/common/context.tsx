cd5f388df788d395a97169563fbfe00824412093
SameerAli678 <alikhansameer234@gmail.com>
2025-07-09 00:18:48 +0500

    'fix' - package.json update - husky update - Create component context in common folder

---
+++ src/components/common/context.tsx
@@ -0,0 +1,12 @@
+// import React, { createContext, ReactNode, useContext, useState } from 'react';
+// const GlobalContext = createContext();
+
+// export const GlobalProvider = (Children: ReactNode) => {
+//   const [card, setCard] = useState(0);
+//   return (
+//     <GlobalContext.Provider value={{ card, setCard }}>
+//       {Children}
+//     </GlobalContext.Provider>
+//   );
+// };
+// export const useGlobalContext = () => useContext(GlobalContext);
