722e05548118240c947b6591b1953dee4edc0fe8
SameerAli678 <alikhansameer234@gmail.com>
2025-11-23 04:50:16 +0500

    "Adit" - Same Interface files Again

--- src/interfaces/properties/my-properties-interface.ts
+++ src/interfaces/properties/my-properties-interface.ts
@@ -1,6 +1,6 @@
 //  src/interfaces/properties/my-properties-interface.tsx
 
- // MyPropertyInterfaces interface Export 
+// MyPropertyInterfaces interface Export
 export interface MyPropertyInterfaces {
   thumbnail: string;
   washRoom: string;
